9adf6628e0628d48535d3796ace99ffb7b4a73ed
Dolphin2Point1 <finb7729@gmail.com>
2021-01-20 12:02:59 -0600

    json block loader

---
+++ BlockModule/src/main/java/com/dolphin2point0/game_stages_fabric/block_module/resources/JsonBlockStageLoader.java
@@ -0,0 +1,50 @@
+package com.dolphin2point0.game_stages_fabric.block_module.resources;
+
+import com.dolphin2point0.game_stages_fabric.GameStagesFabric;
+import com.dolphin2point0.game_stages_fabric.api.resources.MultiJsonDataLoader;
+import com.dolphin2point0.game_stages_fabric.block_module.BlockModule;
+import com.dolphin2point0.game_stages_fabric.block_module.HashMapBlockStageChecker;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.Pair;
+import net.minecraft.util.profiler.Profiler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class JsonBlockStageLoader extends MultiJsonDataLoader implements IdentifiableResourceReloadListener {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    private static final Identifier id = new Identifier("game_stages_fabric", "block_stages");
+
+
+    public JsonBlockStageLoader() {
+        super(GSON, "item_stage");
+    }
+
+    @Override
+    protected void apply(Map<Identifier, List<JsonElement>> loader, ResourceManager manager, Profiler profiler) {
+        BlockModule.BLOCK_STAGE_CHECKER = new HashMapBlockStageChecker();
+        for (Map.Entry<Identifier, List<JsonElement>> entry : loader.entrySet()) {
+            for(JsonElement element : entry.getValue()) {
+                Pair<Block, String[]> deserializedItemStages = BlockStagesSerializer.read(JsonHelper.asObject(element, "top element"));
+                BlockModule.BLOCK_STAGE_CHECKER.addStagesToItem(deserializedItemStages.getLeft(), deserializedItemStages.getRight());
+                GameStagesFabric.LOGGER.info("Added stages " + Arrays.toString(deserializedItemStages.getRight())
+                        + " to block " + deserializedItemStages.getLeft().getTranslationKey());
+            }
+        }
+        GameStagesFabric.LOGGER.info("Added stages to block.");
+    }
+
+    @Override
+    public Identifier getFabricId() {
+        return id;
+    }
+}
