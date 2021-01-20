0ca6f5682191190584e1b17e778a9884ec0c113c
Dolphin2Point1 <finb7729@gmail.com>
2021-01-19 22:08:46 -0600

    initialize block stage checker on startup

--- BlockModule/src/main/java/com/dolphin2point0/game_stages_fabric/block_module/BlockModule.java
+++ BlockModule/src/main/java/com/dolphin2point0/game_stages_fabric/block_module/BlockModule.java
@@ -1,11 +1,13 @@
 package com.dolphin2point0.game_stages_fabric.block_module;
 
+import com.dolphin2point0.game_stages_fabric.block_module.api.BlockStageChecker;
 import net.fabricmc.api.ModInitializer;
 
 public class BlockModule implements ModInitializer {
+    public static BlockStageChecker BLOCK_STAGE_CHECKER;
 
     @Override
     public void onInitialize() {
-
+        BLOCK_STAGE_CHECKER = new HashMapBlockStageChecker();
     }
 }
